61bedc4efa658e6665e0e7e6b7d4a188950ba12c
4mim <2_ven@naver.com>
2025-12-31 09:57:01 +0900

    refactor: refactor fizzbuzz

--- fb_refactor.py
+++ fb_refactor.py
@@ -1,9 +1,7 @@
-for i in range(1, 15 + 1):
-    if i % 15 == 0:
-        print("fizzbuzz")
-    elif i % 3 == 0:
-        print("fizz")
-    elif i % 5 == 0:
-        print("buzz")
+for i in range(1, 16):
+    s = ("fizz" * (i % 3 == 0)) + ("buzz" * (i % 5 == 0))
+    if s:
+        print(s)
     else:
         print(i)
+
